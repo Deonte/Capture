0ef615318dc66718c6d0a3bfc5a2e1d910ae4c57
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-10-23 05:34:31 -0400

    Initial Commit

---
+++ Capture/CaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureApp.swift
+//  Capture
+//
+//  Created by Deonte Kilgore on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
