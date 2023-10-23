175d16ca786702651405f4bab9eb00278b3605e2
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-10-23 05:39:30 -0400

    Created the app structure to provide a good starting point to learning how to use the Photo's library as well as the iOS Devices camera.

--- Capture/Screens/HomeView.swift
+++ Capture/Screens/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  Capture
 //
 //  Created by Deonte Kilgore on 10/23/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
