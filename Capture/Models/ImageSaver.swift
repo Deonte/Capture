379ca68ebdddb14f1432a4df7c6a9223778692ec
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-10-26 06:53:43 -0400

    Created an image saver class to handle saving capturedImages to the photo library.

---
+++ Capture/Models/ImageSaver.swift
@@ -0,0 +1,24 @@
+//
+//  ImageSaver.swift
+//  Capture
+//
+//  Created by Deonte Kilgore on 10/26/23.
+//
+
+import UIKit
+import Photos
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = didFinishSavingWithError {
+            print(error.localizedDescription)
+            return
+        }
+        
+        print("Save Finish")
+    }
+}
